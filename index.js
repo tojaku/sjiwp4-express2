7c8812c673348a528b388066f7b14c529d36a7d1
Tomislav J <tojaku@gmail.com>
2023-10-26 10:27:17 +0200

    Prva promjena

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get("/", (request, response) => {
     response.send("I am alive!");
 });
 
-app.get("/proba/:name", (request, response) => {
-    response.send("Hello, " + request.params.name);
-});
-
 app.listen(port, () => {
     console.log("I am ready!");
 });
